568d533a5ff08521195792f9485226c55b7ce7e2
Matias F Salina <salinamatiasf@gmail.com>
2024-08-06 00:48:06 -0300

    actualizacion de los archivos home, homeprivate, login, profile, register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+// Pagina para manejar el inicio de sesión y redirigir al usuario.
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../services/auth';
+import { useAuth } from '../context/AuthContext';
+import { Alert } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { login: setAuthToken } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await login(username, password);
+      setAuthToken(data.token);
+      navigate('/homeprivate');  // Redirige a HomePrivate
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>Login</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div className="container">
+          <h1>Login</h1>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">Username</label>
+              <input
+                type="text"
+                id="username"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password</label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">Login</button>
+          </form>
+        </div>
+      </main>      
+    </div>
+  );
+};
+
+export default Login;
