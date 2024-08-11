fd1ad7b1923cb85d669060dbc8d593c53f6c7cdd
Matias F Salina <salinamatiasf@gmail.com>
2024-08-11 17:56:04 -0300

    Se agregaron nuevs archivos para el proyecto actualizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,43 @@
-// 
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext'; 
 
-const Navbar = () => {
-  const { isAuthenticated, logout } = useAuth();
+const NavbarComponent = () => {
   const navigate = useNavigate();
+  const { logout, isAuthenticated } = useAuth(); // Función de logout y estado de autenticación
 
-  const handleLogout = async () => {
-    await logout();
-    navigate('/login');
+
+  const handleLogout = () => {
+    logout(); // Llama a la función de logout
+    navigate('/login'); // Redirige al usuario a la página home 
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Mi Recetario</a>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          
-          
-              <li className="nav-item">
-                <button className="btn btn-link nav-link" onClick={handleLogout}>Logout</button>
-              </li>
-            
-        </ul>
-      </div>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/home">Mi Recetario</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/home">Home</Nav.Link>
+          {isAuthenticated && <Nav.Link as={Link} to="/create-recipe">Create Recipe</Nav.Link>}
+          {isAuthenticated ? (
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
+              <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+            </NavDropdown>
+          ) : (
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/register">Register</NavDropdown.Item>
+            </NavDropdown>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavbarComponent;
+
+
