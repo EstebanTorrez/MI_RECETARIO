d83b7a176a1bb7739300e405d850d679b7d41265
Matias F Salina <salinamatiasf@gmail.com>
2024-08-11 17:58:35 -0300

    Se actualizo el archivos para el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import React from 'react';//
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import HomePrivate from './pages/HomePrivate';
+import CreateRecipe from './pages/CreateRecipe';
 import PrivateRoute from './components/PrivateRoute';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    
-        <AuthProvider>
-          <Router>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/homeprivate" element={<PrivateRoute element={<HomePrivate />} />} />
-              <Route path="/profile" element={<PrivateRoute element={<Profile />} />} />
-            </Routes>
-          </Router>  
-        </AuthProvider>
-    
-
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          
+          <Route path="/home" element={<Home />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<PrivateRoute element={<Profile />} />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
 export default App;
+
+
+
