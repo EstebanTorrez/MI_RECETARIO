57ed8c4cb463ce4bcb86793b947afbd5bc58f06b
esteban torres <eantoapolo11@gmail.com>
2024-08-23 03:27:59 -0300

    Se añadio la función getSteps

--- src/services/stepService.js
+++ src/services/stepService.js
@@ -1,7 +1,15 @@
-// Funcion para obtener las valoraciones
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL; 
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const getSteps = async (recipeId) => {
+  try {
+    const response = await axios.get(`${API_URL}/reciperover/steps/?recipe=${recipeId}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response ? error.response.data.detail : 'Error fetching steps');
+  }
+};
 
 export const getRating = async (recipeId) => {
   try {
