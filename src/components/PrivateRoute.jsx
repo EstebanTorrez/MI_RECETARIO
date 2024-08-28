f20d3c8125898288d23c545acd5745e378729a8d
esteban torres <eantoapolo11@gmail.com>
2024-08-28 18:32:55 -0300

    Ajuste de rutas luego de la crecion de AddIngredients.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Route, Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 // Componente de Ruta Privada
-const PrivateRoute = ({ element, ...rest }) => {
+const PrivateRoute = ({ element }) => {
   const { isAuthenticated } = useAuth();
 
   return isAuthenticated ? element : <Navigate to="/login" />;
